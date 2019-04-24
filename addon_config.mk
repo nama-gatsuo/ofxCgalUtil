61e124b0a1b92427849d4357940019088454b43c
ayumu_nagamatsu <ayumu.nagamatsu@gmail.com>
2019-04-25 01:22:37 +0900

    [Update] refactoring lib directory

--- addon_config.mk
+++ addon_config.mk
@@ -8,10 +8,13 @@ meta:
 common:
 
 vs:
-	ADDON_INCLUDES += "boost_1_69_0\include"
-	ADDON_INCLUDES += "CGAL-4.14-beta2\include"
-	ADDON_INCLUDES += "CGAL-4.14-beta2\auxiliary\gmp\include"
-	ADDON_LIBS += "CGAL-4.14-beta2\auxiliary\gmp\lib\libgmp-10.lib"
-	ADDON_LIBS += "CGAL-4.14-beta2\auxiliary\gmp\lib\libmpfr-4.lib"
-	ADDON_LIBS += "boost_1_69_0\libs\ "
-	ADDON_LIBS += "CGAL-4.14-beta2\libs\ "
+	ADDON_INCLUDES += "lib\CGAL-4.14\include"
+	ADDON_INCLUDES += "lib\CGAL-4.14\auxiliary\gmp\include"
+
+	ADDON_LIBS     += "lib\CGAL-4.14\auxiliary\gmp\lib\libgmp-10.lib"
+	ADDON_LIBS     += "lib\CGAL-4.14\auxiliary\gmp\lib\libmpfr-4.lib"
+	ADDON_LIBS     += "lib\CGAL-4.14\libs\ "
+
+	ADDON_DLLS_TO_COPY += "lib\CGAL-4.14\bin\CGAL-vc140-mt-gd-4.14.pdb"
+	ADDON_DLLS_TO_COPY += "lib\CGAL-4.14\bin\CGAL-vc140-mt-gd-4.14.dll"
+	ADDON_DLLS_TO_COPY += "lib\CGAL-4.14\bin\CGAL-vc140-mt-4.14.dll"
