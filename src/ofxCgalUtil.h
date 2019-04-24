4af44f2d5c0d19aaeba5bab30d90185209d187ef
ayumu_nagamatsu <ayumu.nagamatsu@gmail.com>
2019-04-25 00:05:18 +0900

    [Update] refactor library structure

--- src/ofxCgalUtil.h
+++ src/ofxCgalUtil.h
@@ -1,6 +1,13 @@
 #pragma once
+
+#ifdef PI
+#undef PI
+#endif
+
 #include "Types.h"
 #include "PolyhedronConverter.h"
 #include "Triangulation3d.h"
 #include "operation/Slicer.h"
 #include "operation/BooleanOp.h"
+
+#define PI 3.14159265358979323846
