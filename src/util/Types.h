5cb52c3a6e7a81e3f2060c00aa6d8416b148ee17
ayumu_nagamatsu <ayumu.nagamatsu@gmail.com>
2019-04-26 01:16:29 +0900

    [Update] delete unnecessary lines

--- src/util/Types.h
+++ src/util/Types.h
@@ -2,9 +2,6 @@
 #include "CGAL/Exact_predicates_inexact_constructions_kernel.h"
 #include "CGAL/Exact_predicates_exact_constructions_kernel.h"
 #include "CGAL/Polyhedron_3.h"
-
-#include "CGAL/Exact_integer.h"
-#include "CGAL/Homogeneous.h"
 #include "CGAL/Nef_polyhedron_3.h"
 
 namespace ofxCgalUtil {
