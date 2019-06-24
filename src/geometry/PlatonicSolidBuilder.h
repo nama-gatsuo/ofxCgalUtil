7ea97431bf6d9e5a8d93d50e32e42804dee1640b
ayumu_nagamatsu <ayumu.nagamatsu@gmail.com>
2019-06-24 18:47:10 +0900

    [Add] polyhedron data

--- src/geometry/PlatonicSolidBuilder.h
+++ src/geometry/PlatonicSolidBuilder.h
@@ -7,85 +7,94 @@ namespace ofxCgalUtil {
 
 	struct PolyhedronData {
 		enum Type {
-			BOX, TETRAHEDRON, OCTAHEDRON, DODECAHEDRON, ICOSAHEDRON
+			TETRAHEDRON = 0, BOX, OCTAHEDRON, DODECAHEDRON, ICOSAHEDRON
 		};
 		std::vector<std::vector<int>> indices;
 		std::vector<std::vector<float>> points;
-	};
-
-	const static PolyhedronData BoxData{
-		{
-			{ 0, 3, 2, 1 }, { 0, 1, 7, 4 },
-			{ 1, 2, 6, 7 }, { 2, 3, 5, 6 },
-			{ 3, 0, 4, 5 }, { 4, 7, 6, 5 }
-		},
-		{
-			{ -1, 1, -1 }, { 1, 1, -1 }, { 1, 1, 1 }, { -1, 1, 1 },
-			{-1, -1, -1 }, { -1, -1, 1 }, { 1, -1, 1 }, { 1, -1, -1 }
-		}
 	};
 
 	namespace D {
 		const float T = (1. + std::sqrt(5.)) * 0.5;
 		const float R = 1. / T;
 	}
-	
-	const static PolyhedronData DodecahedronData{
+
+	const static std::vector<PolyhedronData> polyData{
 		{
-			{ 3, 11, 7, 15, 13 },
-			{ 7, 19, 17, 6, 15 },
-			{ 17, 4, 8, 10, 6 },
-			{ 8, 0, 16, 2, 10 },
-			{ 0, 12, 1, 18, 16 },
-			{ 6, 10, 2, 13, 15 },
-			{ 2, 16, 18, 3, 13 },
-			{ 18, 1, 9, 11, 3 },
-			{ 4, 14, 12, 0, 8 },
-			{ 11, 9, 5, 19, 7 },
-			{ 19, 5, 14, 4, 17 },
-			{ 1, 12, 14, 5, 9 }
+			{
+				{2, 1, 0}, {0, 3, 2}, {1, 3, 0}, {2, 3, 1}
+			},
+			{
+				{1, 1, 1}, {-1, -1, 1}, {-1, 1, -1}, {1, -1, -1}
+			}
 		},
 		{
-			{ -1, -1, -1}, { -1, -1, 1 }, { -1, 1, -1}, {-1, 1, 1},
-			{ 1, -1, -1}, { 1, -1, 1}, { 1, 1, -1}, { 1, 1, 1},
-			{ 0, -D::R, -D::T}, {0, -D::R, D::T}, { 0, D::R, -D::T }, { 0, D::R, D::T },
-			{ -D::R, -D::T, 0}, {-D::R, D::T, 0}, { D::R, -D::T, 0},  { D::R, D::T, 0},
-			{ -D::T, 0, -D::R}, { D::T, 0, -D::R}, { -D::T, 0, D::R}, {D::T, 0, D::R}
+			{
+				{ 0, 3, 2, 1 }, { 0, 1, 7, 4 },
+				{ 1, 2, 6, 7 }, { 2, 3, 5, 6 },
+				{ 3, 0, 4, 5 }, { 4, 7, 6, 5 }
+			},
+			{
+				{ -1, 1, -1 }, { 1, 1, -1 }, { 1, 1, 1 }, { -1, 1, 1 },
+				{-1, -1, -1 }, { -1, -1, 1 }, { 1, -1, 1 }, { 1, -1, -1 }
+			}
+		},
+		{
+			{
+				{0, 2, 4},	{0, 4, 3},	{0, 3, 5},
+				{0, 5, 2},	{1, 2, 5},	{1, 5, 3},
+				{1, 3, 4},	{1, 4, 2}
+			},
+			{
+				{1, 0, 0 }, {-1, 0, 0},	{0, 1, 0 },
+				{0, -1, 0}, {0, 0, 1 },	{0, 0, -1}
+			}
+		},
+		{
+			{
+				{ 3, 11, 7, 15, 13 }, { 7, 19, 17, 6, 15 },
+				{ 17, 4, 8, 10, 6 }, { 8, 0, 16, 2, 10 },
+				{ 0, 12, 1, 18, 16 }, { 6, 10, 2, 13, 15 },
+				{ 2, 16, 18, 3, 13 }, { 18, 1, 9, 11, 3 },
+				{ 4, 14, 12, 0, 8 }, { 11, 9, 5, 19, 7 },
+				{ 19, 5, 14, 4, 17 }, { 1, 12, 14, 5, 9 }
+			},
+			{
+				{ -1, -1, -1}, { -1, -1, 1 }, { -1, 1, -1}, {-1, 1, 1},
+				{ 1, -1, -1}, { 1, -1, 1}, { 1, 1, -1}, { 1, 1, 1},
+				{ 0, -D::R, -D::T}, {0, -D::R, D::T}, { 0, D::R, -D::T }, { 0, D::R, D::T },
+				{ -D::R, -D::T, 0}, {-D::R, D::T, 0}, { D::R, -D::T, 0},  { D::R, D::T, 0},
+				{ -D::T, 0, -D::R}, { D::T, 0, -D::R}, { -D::T, 0, D::R}, {D::T, 0, D::R}
+			}
+		},
+		{
+			{
+				{0, 11, 5}, {0, 5, 1}, 	{0, 1, 7}, 	{0, 7, 10}, {0, 10, 11},
+				{ 1, 5, 9}, {5, 11, 4},	{11, 10, 2},{10, 7, 6},	{7, 1, 8},
+				{ 3, 9, 4}, {3, 4, 2},	{3, 2, 6},	{3, 6, 8},	{3, 8, 9},
+				{ 4, 9, 5}, {2, 4, 11},	{6, 2, 10},	{8, 6, 7},	{9, 8, 1}
+			},
+			{
+				{-1, D::T, 0}, {1, D::T, 0}, {-1, -D::T, 0}, {1, -D::T, 0},
+				{0, -1, D::T}, {0, 1, D::T}, {0, -1, -D::T}, {0, 1, -D::T},
+				{D::T, 0, -1}, {D::T, 0, 1}, {-D::T, 0, -1}, {-D::T, 0, 1}
+			}
 		}
+		
 	};
-
 
 	template<class HDS>
 	class PlatonicSolidBuilder : public CGAL::Modifier_base<HDS> {
 	public:
-		
-
-		PlatonicSolidBuilder(PolyhedronData::Type type, float size) : size(size), type(type) {}
+		PlatonicSolidBuilder(PolyhedronData::Type type, float size) : 
+			size(size), points(polyData[type].points), indices(polyData[type].indices)
+		{}
 
 		void operator()(HDS& hds) {
 			using Vertex = HDS::Vertex;
 			using Point = Vertex::Point;
 			
 			CGAL::Polyhedron_incremental_builder_3<HDS> builder(hds, true);
-
 			float s = size * scalarForNef / 2.;
-
-			std::vector<std::vector<float>> points;
-			std::vector<std::vector<int>> indices;
-
-			switch (type) {
-			case PolyhedronData::BOX: {
-				points = BoxData.points;
-				indices = BoxData.indices;
-			}break;
-			case PolyhedronData::DODECAHEDRON:{
-				points = DodecahedronData.points;
-				indices = DodecahedronData.indices;
-			}break;
-			default: {
-				ofLogError("ofxCgalUtil::RegularPolyhedronBuilder") << "unsupported type!";
-			} break;
-			}
 
 			builder.begin_surface(points.size(), indices.size());
 			for (auto& p : points) {
@@ -103,7 +112,8 @@ namespace ofxCgalUtil {
 
 	private:
 		const float size;
-		const PolyhedronData::Type type;
+		const std::vector<std::vector<float>> points;
+		const std::vector<std::vector<int>> indices;
 	};
 
 	
